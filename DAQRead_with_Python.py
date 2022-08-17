70ae43ed43a8c1cdaf4a6398992324f688c1bdda
gustavomrv <gustavomourago@gmail.com>
2022-08-17 20:57:16 -0300

    Update DAQRead_with_Python.py

--- DAQRead_with_Python.py
+++ DAQRead_with_Python.py
@@ -2,7 +2,12 @@
 # pip install nidaqmx
 # ==============================================#
 
+from unittest.mock import DEFAULT
 import nidaqmx
 
 task = nidaqmx.Task()
+
+task.ai_channels.add_ai_voltage_chan("dev4/ai0", "myAIChannel", terminal_config=DEFAULT, min_val=-5, max_val=5, units= VoltageUnits)
+
+ 
 #dev1/ai0
