e5163a7a9a0cd9c0f17ca66c6931abeb509c7f03
gustavomrv <gustavomourago@gmail.com>
2022-08-17 21:00:19 -0300

    Update DAQRead_with_Python.py

--- DAQRead_with_Python.py
+++ DAQRead_with_Python.py
@@ -7,7 +7,16 @@ import nidaqmx
 
 task = nidaqmx.Task()
 
-task.ai_channels.add_ai_voltage_chan("dev4/ai0", "myAIChannel", terminal_config=DEFAULT, min_val=-5, max_val=5, units= VoltageUnits)
+task.ai_channels.add_ai_voltage_chan("dev4/ai0", "myAIChannel", terminal_config=DEFAULT, min_val=-5, max_val=5, VoltageUnits = VoltageUnits.VOLTS)
 
- 
+task.start()
+
+value = task.read()
+
+print(value)
+
+task.stop()
+
+task.close()
+
 #dev1/ai0
