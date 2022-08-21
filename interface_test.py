54f646e9bbb5b8c10ce639a694bfc02dbd1acccd
gustavomrv <gustavomourago@gmail.com>
2022-08-21 14:25:45 -0300

    Update interface_test.py

--- interface_test.py
+++ interface_test.py
@@ -3,15 +3,15 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-Tstop = 10 # Logging Time [seconds]
-Ts = 1 # Sampling Time [seconds]
-data = [1,2,2,1,3,4,5,6,10,3]
+Tstop = 25 # Logging Time [seconds]
+Ts = 5# Sampling Time [seconds]
+data = [0,4,12,17,0]
 t = np.arange(0,Tstop,Ts)
 plt.plot(t,data, "-o")
-plt.title('Temperature')
-plt.xlabel('t [s]')
-plt.ylabel('Temp [degC]')
+plt.title('Curvas Características')
+plt.xlabel('Tensão [V]')
+plt.ylabel('Potência [W]')
 plt.grid()
-Tmin = 0; Tmax = 10.5
+Tmin = 0; Tmax = 20
 plt.axis([0, Tstop, Tmin, Tmax])
 plt.show()
