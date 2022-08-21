84c3655f94411ce452523b0f8efca5102b706e37
gustavomrv <gustavomourago@gmail.com>
2022-08-21 16:06:22 -0300

    Update interface_test.py

--- interface_test.py
+++ interface_test.py
@@ -3,10 +3,10 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-Tstop = 25 # Logging Time [seconds]
-Ts = 5# Sampling Time [seconds]
-data = [0,4,12,17,0]
-t = np.arange(0,Tstop,Ts)
+#Tstop = 25 # Logging Time [seconds]
+#Ts = 5# Sampling Time [seconds]
+#data = [0,4,12,17,0]
+#t = np.arange(0,Tstop,Ts)
 
 tensao = []
 potencia = []
@@ -20,12 +20,12 @@ plt.plot(tensao,potencia, "")
 plt.title('Curvas Características')
 plt.xlabel('Tensão [V]')
 plt.ylabel('Potência [W]')
-plt.grid()
+# plt.grid()
 
 print(max(tensao))
 print(max(potencia))
 
-Tmin = 0; Tmax = potencia[24]
+# Tmin = 0; Tmax = potencia[24]
 
-plt.axis([0, Tstop, Tmin, Tmax])
+plt.axis([0, max(tensao) + 6, 0, max(potencia)+100])
 plt.show()
